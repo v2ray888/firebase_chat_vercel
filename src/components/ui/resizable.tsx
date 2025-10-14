103f9253b30bc9cb200c09201a699a4d8b66c836
hai chen <v2ray888@gmail.com>
2025-10-14 22:46:13 +0000

    Try fixing this error: `Runtime Error: Error: Element type is invalid: e

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { ImperativePanelHandle, Panel, PanelGroup } from "react-resizable-panels"
+import { ImperativePanelHandle, Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
 
@@ -24,10 +24,10 @@ const ResizableHandle = ({
   withHandle,
   className,
   ...props
-}: React.ComponentProps<typeof PanelGroup.Handle> & {
+}: React.ComponentProps<typeof PanelResizeHandle> & {
   withHandle?: boolean
 }) => (
-  <PanelGroup.Handle
+  <PanelResizeHandle
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=horizontal]]:cursor-col-resize [&[data-panel-group-direction=vertical]]:cursor-row-resize",
       className
@@ -39,7 +39,7 @@ const ResizableHandle = ({
         <div className="h-2 w-px bg-muted-foreground" />
       </div>
     )}
-  </PanelGroup.Handle>
+  </PanelResizeHandle>
 )
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
